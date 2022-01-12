1b4c3b024bd89722b1b8cb686e953ba7393f7ff3
AlexKorshakov <korshakov_07@mail.ru>
2022-01-12 03:54:46 +0300

    V 1.01 update send_mail_handler

--- handlers/send_mail/send_mail_handler.py
+++ handlers/send_mail/send_mail_handler.py
@@ -54,7 +54,7 @@ async def send_mail(message: types.Message, file_list: list = None, registration
     # assert isinstance(SENT_TO_CC, list)
 
     if not file_list:
-        file_list = await get_report_file_list(chat_id=message.from_user.id)
+        file_list = await get_report_file_list(chat_id=message.from_user.id, endswith='.pdf')
         if not file_list:
             logger.warning(Messages.Error.file_list_not_found)
             await bot.send_message(message.from_user.id, Messages.Error.file_list_not_found)
@@ -80,15 +80,30 @@ async def send_mail(message: types.Message, file_list: list = None, registration
         await bot.send_message(message.from_user.id, Messages.Error.location_name_not_found)
         return
 
-    sent_to = None
+    sent_to = []
     if location in [list(item.keys())[0] for item in METRO_STATION]:
 
         for item in METRO_STATION:
             if list(item.keys())[0] == location:
-                sent_to: list = item.get(location)
+                dikt_sent_to: list = item.get(location)
 
-                break  # strjuchkovav@mosinzhproekt.ru
+                for item_to in dikt_sent_to:
+                    if list(item_to.keys())[0] == 'RS':
+                        rs_list = item_to.get('RS')
+                        sent_to.append(rs_list)
 
+                    if list(item_to.keys())[0] == 'HSE':
+                        hse_list = item_to.get('HSE')
+                        for i in hse_list:
+                            sent_to.append(i)
+
+                    if list(item_to.keys())[0] == 'SUB_HSE':
+                        sub_hse_list = item_to.get('SUB_HSE')
+                        for i in sub_hse_list:
+                            sent_to.append(i)
+
+                break
+                # strjuchkovav@mosinzhproekt.ru
                 # // "arsutinaa@mosinzhproekt.ru",
                 # // "lozhkov.rd@mosinzhproekt.ru",
                 # // "Nikolaev.SM@mosinzhproekt.ru",
@@ -195,3 +210,126 @@ async def send_mail(message: types.Message, file_list: list = None, registration
     else:
         logger.error(Messages.Error.no_file_too_send)
         await bot.send_message(message.from_user.id, Messages.Error.no_file_too_send)
+
+
+if __name__ == '__main__':
+
+    location = "Ст. Аминьевская"
+
+    sent_to = []
+    if location in [list(item.keys())[0] for item in METRO_STATION]:
+
+        for item in METRO_STATION:
+            if list(item.keys())[0] == location:
+                dikt_sent_to: list = item.get(location)
+
+                for item_to in dikt_sent_to:
+                    if list(item_to.keys())[0] == 'RS':
+                        rs_list = item_to.get('RS')
+                        sent_to.append(rs_list)
+
+                    if list(item_to.keys())[0] == 'HSE':
+                        hse_list = item_to.get('HSE')
+                        for i in hse_list:
+                            sent_to.append(i)
+
+                    if list(item_to.keys())[0] == 'SUB_HSE':
+                        sub_hse_list = item_to.get('SUB_HSE')
+                        for i in sub_hse_list:
+                            sent_to.append(i)
+
+    # email_message = MIMEMultipart('mixed')
+    # email_message['To'] = ', '.join(sent_to)
+
+    # sent_to = [i for row in sent_to for i in row]
+    sent_to = list(set(sent_to))
+    print(sent_to)
+
+    email_message = MIMEMultipart('mixed')
+    email_message['From'] = f"{SENDER} <{SENDER_ACCOUNT_GMAIL}>"
+    email_message['To'] = ', '.join(sent_to)
+    email_message['Cc'] = ', '.join(SENT_TO_CC)
+
+    file_list = ['C:\\Users\\KDeusEx\\Downloads\\WhatsApp Image 2022-01-09 at 21.44.22.jpeg']
+    isreportfile = []
+    for report_file in file_list or []:
+        if not os.path.isfile(report_file):
+            logger.error(f"report_file {report_file} {Messages.Error.file_not_found}")
+            isreportfile.append(False)
+            continue
+
+        mime_type, _ = mimetypes.guess_type(report_file)
+        mime_type, mime_subtype = mime_type.split('/')
+
+        try:
+            with open(report_file, "rb") as attachment:
+                p = MIMEApplication(attachment.read(), _subtype=mime_subtype)
+                name = report_file.split("\\")[-1]
+
+                p.add_header('Content-Disposition', 'attachment', filename=name)
+                email_message.attach(p)
+                isreportfile.append(True)
+                break
+        except Exception as e:
+            print(str(e))
+            # return
+
+    if any([file for file in isreportfile]):
+
+        # await bot.send_message(message.from_user.id, Messages.Successfully.letter_formed)
+        logger.info(Messages.Successfully.letter_formed)
+
+        custom_date = datetime.now().strftime("%d.%m.%Y")
+        date_now = str(datetime.now().strftime("%d.%m.%Y"))
+        date_then = datetime.now() - timedelta(days=1)
+        date_then = str(date_then.strftime("%d.%m.%Y"))
+
+        registration_data: dict = {}
+
+        location = registration_data["name_location"] if registration_data.get('name_location') else ''
+        work_shift = registration_data["work_shift"] if registration_data.get('work_shift') else ''
+        function = registration_data["function"] if registration_data.get('function') else ''
+        name = registration_data["name"] if registration_data.get('name') else ''
+
+        if registration_data.get('work_shift'):
+            if work_shift.lower() == "дневная смена":
+                work_shift = 'дневной смены'
+                custom_date = f"{date_now}"
+            else:
+                work_shift = ' о ночной смене'
+                custom_date = f"{date_then} - {date_now}"
+        else:
+            work_shift = ''
+
+        email_message['Subject'] = f'Отчет за {custom_date} по площадке {location}'
+        msg_content = f'<h4>Добрый день!<br> Направляю Вам Отчет {work_shift} за {custom_date} ' \
+                      f'по площадке {location}.<br>' \
+                      f'Отчет составил {function} {name}.</h4>' \
+                      f'<br>' \
+                      f'<br>' \
+                      f'</center>'
+        # f'по вопросам работы бота обращайтесь {DEVELOPER_EMAIL}' \
+        body = MIMEText(msg_content, 'html')
+        email_message.attach(body)
+
+        msg_full = email_message.as_string()
+
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.ehlo()
+            server.starttls(context=context)
+            server.ehlo()
+            server.login(SENDER_ACCOUNT_GMAIL, password=SENDER_ACCOUNT_PASSWORD)
+            server.sendmail(SENDER_ACCOUNT_GMAIL,
+                            to_addrs=sent_to + SENT_TO_CC,
+                            msg=msg_full)
+            # await bot.send_message(message.from_user.id, Messages.Successfully.mail_send)
+            logger.info(Messages.Successfully.mail_send)
+            server.quit()
+
+        # await asyncio.sleep(10)
+
+    else:
+        logger.error(Messages.Error.no_file_too_send)
+        # await bot.send_message(message.from_user.id, Messages.Error.no_file_too_send)
