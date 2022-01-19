22c4c2f185ca3c793ca3f67013d8d810cda44a27
AlexK <korshakov_07@mail.ru>
2022-01-19 03:25:04 +0300

    to pc

--- callbacks/sequential_action/__init__.py
+++ callbacks/sequential_action/__init__.py
@@ -1,8 +1,8 @@
 
+from . import main_category_answer
+from . import category_answer
 from . import general_contractors_answer
 from . import violation_category_answer
-from . import category_answer
-from . import main_category_answer
 from . import incident_level_answer
 from . import act_required_answer
 from . import elimination_time_answer
